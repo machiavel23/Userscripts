0c7e881b6f01ed8c72d8f5cefdd71cdaaefc34d3
machiavel23 <30846033+machiavel23@users.noreply.github.com>
2024-01-20 23:00:26 +0100

    Create NMPVU (Naughty Machinima Private Videos Unlocker).meta.js
    Added méta for nmpvu

---
+++ NMPVU (Naughty Machinima Private Videos Unlocker).meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         NMPVU (Naughty Machinima Private Videos Unlocker)
+// @namespace    https://www.naughtymachinima.com
+// @version      0.2
+// @description  Adds a link to access private videos
+// @author       Machiavel Dhy'v
+// @match        https://www.naughtymachinima.com/video/*
+// @grant        none
+// ==/UserScript==
