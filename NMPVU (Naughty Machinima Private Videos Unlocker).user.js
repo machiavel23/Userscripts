8f2863d2e134d128c9ffdfce0d26c6325aeebf07
machiavel23 <30846033+machiavel23@users.noreply.github.com>
2024-01-20 22:34:14 +0100

    Update NMPVU (Naughty Machinima Private Videos Unlocker).user.js
    Updated hook for new NM looks.

--- NMPVU (Naughty Machinima Private Videos Unlocker).user.js
+++ NMPVU (Naughty Machinima Private Videos Unlocker).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NMPVU (Naughty Machinima Private Videos Unlocker)
 // @namespace    https://www.naughtymachinima.com
-// @version      0.1
+// @version      0.2
 // @description  Adds a link to access private videos
 // @author       Machiavel Dhy'v
 // @match        https://www.naughtymachinima.com/video/*
@@ -25,15 +25,12 @@
         // Build new url
         var newURL = 'https://www.naughtymachinima.com/media/videos/h264/' + capturedPart;
 
-        // Look for div with "text-danger" class
-        var textDangerDiv = document.querySelector('.text-danger');
+        // Look for span with "text-danger" class
+        var textDanger = document.querySelector('span.text-danger');
 
-        // Create our own div with the new url and insert it before the "text-danger" div
-        if (textDangerDiv) {
-            var newDiv = document.createElement('div');
-            newDiv.className = 'well ad-body';
-            newDiv.innerHTML = 'View the video with NMPVU userscript:<br><a href="' + newURL + '_1080p.mp4">1080p</a> - <a href="' + newURL + '_720p.mp4">720p</a> - <a href="' + newURL + '_480p.mp4">480p</a> - <a href="' + newURL + '_360p.mp4">360p</a><br>Keep in mind not all videos have been uploaded in HD. Try a lower resolution if you reach a 404.';
-            textDangerDiv.parentNode.insertBefore(newDiv, textDangerDiv);
+        // Adding links in textDanger
+        if (textDanger) {
+            textDanger.innerHTML += '<br><br>View the video with NMPVU userscript:<br><a href="' + newURL + '_1080p.mp4">1080p</a> - <a href="' + newURL + '_720p.mp4">720p</a> - <a href="' + newURL + '_480p.mp4">480p</a> - <a href="' + newURL + '_360p.mp4">360p</a><br>Keep in mind not all videos have been uploaded in HD. Try a lower resolution if you reach a 404.';
         }
     }
 })();
