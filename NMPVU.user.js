9ea49bc63f3ee3bd49a59bbd3feff4257ce298de
machiavel23 <30846033+machiavel23@users.noreply.github.com>
2024-01-26 23:53:32 +0100

    Update NMPVU.user.js
    Added unblurring for miniatures

--- NMPVU.user.js
+++ NMPVU.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NMPVU (Naughty Machinima Private Videos Unlocker)
 // @namespace    https://www.naughtymachinima.com
-// @version      0.2
+// @version      0.3
 // @description  Adds a link to access private videos
 // @author       Machiavel Dhy'v
 // @match        https://www.naughtymachinima.com/*
@@ -12,32 +12,28 @@
 
 (function() {
     'use strict';
-    
-    // Sélectionner tous les éléments <img> avec la classe "img-responsive img-private"
-    var imgElements = document.querySelectorAll('img.img-responsive.img-private');
 
-    // Parcourir tous les éléments sélectionnés
+    // UNBLURRING PRIVATE MINIATURES
+    // Getting <img class="img-responsive img-private">
+    var imgElements = document.querySelectorAll('img.img-responsive.img-private');
     imgElements.forEach(function(imgElement) {
-        // Modifier la classe en "img-responsive"
+        // Removing blur
         imgElement.classList.remove('img-private');
 });
+    
+    // ENABLING PRIVATE VIDEOS
     // Get current url
     var currentURL = window.location.href;
-
     // Get the digits between "/video/" and the next "/"
     var regex = /\/video\/([^\/]+)\//;
     var match = currentURL.match(regex);
-
     if (match) {
-        // Extract the digits
+        // Extracting digits
         var capturedPart = match[1];
-
         // Build new url
         var newURL = 'https://www.naughtymachinima.com/media/videos/h264/' + capturedPart;
-
         // Look for span with "text-danger" class
         var textDanger = document.querySelector('span.text-danger');
-
         // Adding links in textDanger
         if (textDanger) {
             textDanger.innerHTML += '<br><br>View the video with NMPVU userscript:<br><a href="' + newURL + '_1080p.mp4" target="_blank">1080p</a> - <a href="' + newURL + '_720p.mp4" target="_blank">720p</a> - <a href="' + newURL + '_480p.mp4" target="_blank">480p</a> - <a href="' + newURL + '_360p.mp4" target="_blank">360p</a><br>Keep in mind not all videos have been uploaded in HD. Try a lower resolution if you reach a 404.';
