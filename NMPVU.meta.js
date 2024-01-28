dd48349fbb55159829733ed49b0045092fe19ce6
machiavel23 <30846033+machiavel23@users.noreply.github.com>
2024-01-28 20:12:11 +0100

    Delete NMPVU.meta.js

--- NMPVU.meta.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         NMPVU (Naughty Machinima Private Videos Unlocker)
-// @namespace    https://www.naughtymachinima.com
-// @version      0.3
-// @description  Adds a link to access private videos
-// @author       Machiavel Dhy'v
-// @match        https://www.naughtymachinima.com/*
-// @grant        none
-// ==/UserScript==
